9158ba3601fdb441b80a5139317bd9bf33e3de4a
aayanqazi <aayanqazi@yahoo.com>
2017-08-01 14:54:06 +0500

    Implemented background screen of login

--- app/component/login/index.js
+++ app/component/login/index.js
@@ -1,37 +1,60 @@
 import React, { Component } from 'react';
 import {
-  AppRegistry,
-  StyleSheet,
-  View
+    AppRegistry,
+    StyleSheet,
+    View,
+    Image
 } from 'react-native';
-import { Container, Header,Form,Item,Input, Title, Content, Footer, FooterTab, Button, Left, Right, Body, Icon, Text } from 'native-base';
+import { Container, Header, Form, Thumbnail, Item, Input, Title, Content, Footer, FooterTab, Button, Left, Right, Body, Icon, Text } from 'native-base';
 
 export default class Login extends Component {
-  render() {
-    return (
-      <Container>
-        <Header>
-            <Left>
-            <Button transparent>
-              <Icon name='menu' />
-            </Button>
-          </Left>
-             <Body>
-            <Title>Authentication</Title>
-            </Body>
-            </Header>
-        <Content>
-          <Form>
-            <Item>
-              <Input placeholder="Email" />
-            </Item>
-            <Item last>
-              <Input placeholder="Password" />
-            </Item>
-          </Form>
-        </Content>
-      </Container>
+    render() {
+        return (
+            <Container>
+                <Header>
+                    <Left>
+                        <Button transparent>
+                            <Icon name='menu' />
+                        </Button>
+                    </Left>
+                    <Body>
+                        <Title>Authentication</Title>
+                    </Body>
+                </Header>
+                <Image style={Style.backImage} source={ require("../../assets/back.png")  }>
+                <Content>
+                    <View style={Style.imageStyle}>
+                        <Thumbnail
+                            style={Style.imageThumbnail}
+                            source={ require("../../assets/doctor.png")}
+                        />
+                    </View>
+                    <Form>
+                        <Item>
+                            <Input placeholder="Email" />
+                        </Item>
+                        <Item last>
+                            <Input placeholder="Password" />
+                        </Item>
+                    </Form>
+                </Content>
+                </Image>
+            </Container>
 
-    );
-  }
+        );
+    }
+}
+const Style = {
+    imageThumbnail: {
+        width: 200,
+        height: 200,
+        alignItems: 'center'
+    },
+    imageStyle: {
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    backImage: {
+       flex: 1, width: null, height: null 
+    }
 }
