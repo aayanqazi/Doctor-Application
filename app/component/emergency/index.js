1e9a450083754fa6bf30bbb9772ccfc205efc46f
aayanqazi <aayanqazi@yahoo.com>
2017-08-04 21:01:45 +0500

    Partially implemented emergency number

--- app/component/emergency/index.js
+++ app/component/emergency/index.js
@@ -4,22 +4,39 @@ import {
     Text,
     StyleSheet,
     Image,
+    ListView
 } from "react-native";
-import {Container} from "native-base";
+import { Container, Content, Button, Icon, List, ListItem } from "native-base";
 import Header from "../header/";
-
-
-
+const datas = [
+    'Ambulance: 115',
+    'Edhi Trust: 32310066',
+    'Bomb Disposal: 32416626',
+    'Civil Defence: 37728311',
+    'Sui Gas Complaints: 4934881',
+    'Police Emergency: 15',
+    'News Agency(APP): 3210281',
+    'Hospital Civil(casualties): 37729719'
+];
 export default class Emergency extends Component {
+
     constructor(props) {
         super(props);
+        const ds = new ListView.DataSource({ rowHasChanged: (r1, r2) => r1 !== r2 });
+        this.state = {
+            basic: true,
+            dataSource: ds.cloneWithRows(datas),
+        };
     }
     render() {
+
         return (
             <Container>
                 <Image style={Style.backImage} source={require("../../assets/back.png")} >
-                    <Header name="Emergency Number" iconName="arrow-back" clickEvent={()=>this.props.navigation.goBack()}/>
-                    <Text>Emergency Numbers</Text>
+                    <Header name="Emergency Number" iconName="arrow-back" clickEvent={() => this.props.navigation.goBack()} />
+                    <Content >
+                        <ListView style={{flex:1}} dataSource={this.state.dataSource} renderRow={(rowData) => <Text>{rowData}</Text>} />
+                    </Content>
                 </Image>
             </Container>
         )
